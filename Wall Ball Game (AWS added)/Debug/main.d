4d8b53cac2c6d42edced1c9f6aabcd110c02e240
Raymond Chan <rwfchan@ucdavis.edu>
2020-09-09 13:00:29 -0700

    renaming lab4 to WBG(AWS added)

---
+++ Wall Ball Game (AWS added)/Debug/main.d
@@ -0,0 +1,87 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/string.h
+main.obj: C:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/_ti_config.h
+main.obj: C:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/linkage.h
+main.obj: C:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/cdefs.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/simplelink.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/../user.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/../cc_pal.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink_extlib/provisioninglib/provisioning_api.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/simplelink.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/../source/objInclusion.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/trace.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/fs.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/socket.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/netapp.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/wlan.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/device.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/netcfg.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/wlan_rx_filters.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/../source/nonos.h
+main.obj: C:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdio.h
+main.obj: C:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdarg.h
+main.obj: C:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/_types.h
+main.obj: C:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/machine/_types.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_types.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_memmap.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_common_reg.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_ints.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/spi.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom_map.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom_patch.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/interrupt.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/prcm.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/utils.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/uart.h
+main.obj: ../pin_mux_config.h
+main.obj: ../common.h
+main.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/example/common/uart_if.h
+main.obj: ../i2c_if.h
+main.obj: ../Adafruit_SSD1351.h
+main.obj: ../glcdfont.h
+
+../main.c: 
+C:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/string.h: 
+C:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/_ti_config.h: 
+C:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/linkage.h: 
+C:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/cdefs.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/simplelink.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/../user.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/../cc_pal.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink_extlib/provisioninglib/provisioning_api.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/simplelink.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/../source/objInclusion.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/trace.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/fs.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/socket.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/netapp.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/wlan.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/device.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/netcfg.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/wlan_rx_filters.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/simplelink/include/../source/nonos.h: 
+C:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdio.h: 
+C:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdarg.h: 
+C:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/_types.h: 
+C:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/machine/_types.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_types.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_memmap.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_common_reg.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_ints.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/spi.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom_map.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom_patch.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/interrupt.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/prcm.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/utils.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/uart.h: 
+../pin_mux_config.h: 
+../common.h: 
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/example/common/uart_if.h: 
+../i2c_if.h: 
+../Adafruit_SSD1351.h: 
+../glcdfont.h: 
---
+++ Wall Ball Game (AWS added)/Release/uart_if.d
@@ -0,0 +1,41 @@
+# FIXED
+
+uart_if.obj: ../uart_if.c
+uart_if.obj: D:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdarg.h
+uart_if.obj: D:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/_types.h
+uart_if.obj: D:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/cdefs.h
+uart_if.obj: D:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/_ti_config.h
+uart_if.obj: D:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/linkage.h
+uart_if.obj: D:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/machine/_types.h
+uart_if.obj: D:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdlib.h
+uart_if.obj: D:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdio.h
+uart_if.obj: D:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/string.h
+uart_if.obj: D:/TI/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_types.h
+uart_if.obj: D:/TI/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_memmap.h
+uart_if.obj: D:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/prcm.h
+uart_if.obj: D:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/pin.h
+uart_if.obj: D:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/uart.h
+uart_if.obj: D:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom.h
+uart_if.obj: D:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom_map.h
+uart_if.obj: D:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom_patch.h
+uart_if.obj: D:/TI/CC3200SDK_1.5.0/cc3200-sdk/example/common/uart_if.h
+
+../uart_if.c: 
+D:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdarg.h: 
+D:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/_types.h: 
+D:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/cdefs.h: 
+D:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/_ti_config.h: 
+D:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/linkage.h: 
+D:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/machine/_types.h: 
+D:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdlib.h: 
+D:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdio.h: 
+D:/TI/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/string.h: 
+D:/TI/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_types.h: 
+D:/TI/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_memmap.h: 
+D:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/prcm.h: 
+D:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/pin.h: 
+D:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/uart.h: 
+D:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom.h: 
+D:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom_map.h: 
+D:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom_patch.h: 
+D:/TI/CC3200SDK_1.5.0/cc3200-sdk/example/common/uart_if.h: 
